7ee4df88a80a21b15cefc34b243170be446504de
Aashish Dahale <dahale22@gmail.com>
2025-01-09 09:13:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //use newer version of JS
+
+// alert(3+3)
+//we are using nodeJs not Browser
+
+console.log(3 + 3)
+console.log("aashish")
+
+let name = "Aashish"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null =>standalone value
+//undefined
+//symbol => unique
+
+
+//object
+
+console.log(typeof(age))
+console.log(typeof null)    //object
+console.log(typeof undefined) //undefined
+
+
