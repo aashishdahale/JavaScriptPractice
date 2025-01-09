8f2ee3444cce8da27c511e6a0d0495945342722e
Aashish Dahale <dahale22@gmail.com>
2025-01-09 07:56:30 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 10001
+let accountEmail = "somthing@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+// accountId = 2
+accountEmail = "new@gmail.com"
+accountPassword = "121212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
